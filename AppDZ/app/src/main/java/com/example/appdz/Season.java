f7ce8b996a802a123a98f038604842ae86821fbd
SergSerg <sergio.ne@inbox.ru>
2020-07-26 19:32:57 +0600

    Добавил обработку в активити

--- AppDZ/app/src/main/java/com/example/appdz/Season.java
+++ AppDZ/app/src/main/java/com/example/appdz/Season.java
@@ -1,4 +1,31 @@
 package com.example.appdz;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Season {
+
+    @SerializedName("time_of_year")
+    @Expose
+    private String timeOfYear;
+    @SerializedName("year")
+    @Expose
+    private Integer year;
+
+    public String getTimeOfYear() {
+        return timeOfYear;
+    }
+
+    public void setTimeOfYear(String timeOfYear) {
+        this.timeOfYear = timeOfYear;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
 }
