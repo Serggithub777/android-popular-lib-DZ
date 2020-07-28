43cecde845784a868a19f1f676329ee190b2f77a
SergSerg <sergio.ne@inbox.ru>
2020-07-28 22:33:09 +0600

    Создал класс User

---
+++ AppDZ/app/src/main/java/com/example/appdz/User.java
@@ -0,0 +1,42 @@
+package com.example.appdz;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("login")
+    @Expose
+    private String login;
+    @SerializedName("avatar_url")
+    @Expose
+    private String avatarUrl;
+    @SerializedName("company")
+    @Expose
+    private String company;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+}
