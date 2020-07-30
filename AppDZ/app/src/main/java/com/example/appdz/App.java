46db078582972aef0831536e8d10be566cf1ce32
SergSerg <sergio.ne@inbox.ru>
2020-07-30 19:07:49 +0600

    Создал класс App

--- AppDZ/app/src/main/java/com/example/appdz/App.java
+++ AppDZ/app/src/main/java/com/example/appdz/App.java
@@ -2,6 +2,19 @@ package com.example.appdz;
 
 import android.app.Application;
 
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
 public class App extends Application {
 private static AppDatabase appDatabase;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appDatabase = Room.databaseBuilder(getApplicationContext(),
+                                          AppDatabase.class,"users_database").build();
+
+    }
+
+    public static AppDatabase getAppDatabase(){return appDatabase;}
 }
