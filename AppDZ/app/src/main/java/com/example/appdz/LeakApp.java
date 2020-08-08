51e35c6284696dd463962028ef778e953eeb81ce
SergSerg <sergio.ne@inbox.ru>
2020-08-08 13:57:56 +0600

    Создал Class LeakApp для инициализации LeakCanary.

---
+++ AppDZ/app/src/main/java/com/example/appdz/LeakApp.java
@@ -0,0 +1,19 @@
+package com.example.appdz;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class LeakApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)){
+
+            return;
+        }
+
+          LeakCanary.install(this);
+    }
+}
